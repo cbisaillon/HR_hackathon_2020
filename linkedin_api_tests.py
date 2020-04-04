5684039eaf107804798eb90e23fbcb7c808bbcc4
Clement <clement.bisaillon@hotmail.com>
2020-04-04 17:30:31 -0400

    tests

--- linkedin_api_tests.py
+++ linkedin_api_tests.py
@@ -1,6 +1,13 @@
+from linkedin import server
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 
 def main():
-    pass
+    application = server.quick_api(os.getenv("LINKEDIN_CLIENT_ID"),
+                                   os.getenv("LINKEDIN_SECRET"))
+
 
 if __name__ == "__main__":
     main()
